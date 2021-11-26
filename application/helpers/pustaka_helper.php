0cac3b3c0cc12426f6d6f0b4615fa9a749340b56
abdullahindranugraha <indranugraha1919@gmail.com>
2021-11-26 20:44:15 +0700

    revisi pustaka helper

--- application/helpers/pustaka_helper.php
+++ application/helpers/pustaka_helper.php
@@ -7,5 +7,17 @@ function cek_login()
         redirect('autentifikasi');
     } else {
         $role_id = $ci->session->userdata('role_id');
+        $id_user = $ci->session->userdata('id_user');
+    }
+}
+
+function cek_user()
+{
+    $ci = get_instance();
+
+    $role_id = $ci->session->userdata('role_id');
+    if ($role_id != 1) {
+        $ci->session->set_flashdata('pesan', '<div class="alert alert-danger" role="alert">Akses tidak diizinkan </div>');
+        redirect('autentifikasi');
     }
 }
